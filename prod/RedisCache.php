48d8ed5ffa11126e3dbd19c3e1284a2dfa54e3d2
Yaoguais <897622703@qq.com>
2015-07-09 06:43:52 +0800

    design

---
+++ prod/RedisCache.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace minions;
+
+class RedisCache implements ICache{
+
+    public function increase($key, $step = 1) {
+
+    }
+
+    public function decrease($key, $step = 1) {
+
+    }
+
+    public function setMap($key, $map) {
+
+    }
+
+    public function getMap($key, &$map) {
+
+    }
+
+    public function addToSet($key, $value) {
+
+    }
+
+    public function removeFromSet($key, &$value) {
+
+    }
+
+    public function pushToList($key, $value) {
+
+    }
+
+    public function popFromList($key, &$value) {
+
+    }
+
+    public function getList($key, $start, $end = -1) {
+
+    }
+}
