387fb8dbb862946a4cff1c8d08222f30b669ca31
Yaoguais <newtopstdio@163.com>
2016-01-14 15:16:18 +0800

    rename minions to jegarn

--- app/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('UPLOAD_PATH', __DIR__. '/upload');
-define('APPLICATION_PATH', substr(__FILE__, 0, -17));
-$application = new \Yaf\Application(APPLICATION_PATH . "/../config/application.ini");
-$application->bootstrap()->run();
--- tests/app/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('TEST_HOST', 'http://192.168.199.243');
-require __DIR__ . '/AppTestBase.php';
-require __DIR__ . '/../../src/bootstrap.php';
-
-$config = new \Yaf\Config\Ini(__DIR__ . '/../../config/application.ini','develop');
-\minions\manager\DbManager::getInstance()->initConfig($config->get('application')->get('database'));
