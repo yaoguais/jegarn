2e3026b408d1cbff73a7a73be4a48742dce05966
Yaoguais <newtopstdio@163.com>
2016-04-29 21:41:14 +0800

    make ios-chat-system start

---
+++ examples/ios-chat-system/minions/Podfile
@@ -0,0 +1,4 @@
+platform :ios
+pod 'Reachability',  '~> 3.0.0'
+pod 'ASIHTTPRequest'
+pod 'RegexKitLite'
---
+++ examples/ios-chat-system/minions/minions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  minions
+//
+//  Created by 刘勇 on 16/4/26.
+//  Copyright © 2016年 KyleLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
